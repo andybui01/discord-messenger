425be688ce35738e659da1c98870f7346893e231
andybui01 <andy.bui2001@gmail.com>
2019-12-14 21:51:21 +1100

    add replies from fb to discord

--- helpers.js
+++ helpers.js
@@ -1,14 +1,139 @@
 const fs = require('fs');
 const yaml = require('js-yaml');
 
-exports.writeMessage = function(body, reacts, senderID, is_fb) {
+// Convert id to database-friendly msg_id
+exports.IdToMsgId = function(id) {
+    // Read messages file into js object
     let messages = yaml.load(fs.readFileSync('db/messages.yaml', 'utf8'));
+
+    // Find platform_id
+    var i;
+    var ret = null
+    for (i = messages.numMessages - 1; i >= 0; i--) {
+        if (messages.messages[i]["platform_id"]["fb_id"] == id || messages.messages[i]["platform_id"]["dc_id"] == id) {
+            ret = messages.messages[i]["msg_id"];
+            break;
+        }
+    }
+
+    // Write to file
+    fs.writeFileSync('db/messages.yaml', yaml.dump(messages), (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+
+    return ret
+}
+
+// Converts msg_id to platform id requested, returns dictionary to unpack
+exports.MsgIdToId = function(msg_id) {
+    // Read messages file into js object
+    let messages = yaml.load(fs.readFileSync('db/messages.yaml', 'utf8'));
+
+    // Find platform_id
+    var i;
+    var ret = null
+    for (i = messages.numMessages - 1; i >= 0; i--) {
+        if (messages.messages[i]["msg_id"] == msg_id) {
+            ret = messages.messages[i]["platform_id"];
+            break;
+        }
+    }
+
+    // Write to file
+    fs.writeFileSync('db/messages.yaml', yaml.dump(messages), (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+
+    return ret
+}
+
+// Convert message content to msg_id
+exports.contentToMsgId = function(content) {
+    // Read messages file into js object
+    let messages = yaml.load(fs.readFileSync('db/messages.yaml', 'utf8'));
+
+    // Find platform_id
+    var i;
+    var ret = null
+    for (i = messages.numMessages - 1; i >= 0; i--) {
+        if (content == messages.messages[i]["body"]) {
+            ret = messages.messages[i]["msg_id"];
+            break;
+        }
+    }
+
+    // Write to file
+    fs.writeFileSync('db/messages.yaml', yaml.dump(messages), (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+
+    return ret
+}
+
+exports.setFacebookId = function(msg_id, fb_id) {
+    // Read messages file into js object
+    let messages = yaml.load(fs.readFileSync('db/messages.yaml', 'utf8'));
+
+    // Reset
+    var i;
+    for (i = messages.numMessages - 1; i >= 0; i--) {
+        if (messages.messages[i]["msg_id"] == msg_id) {
+            messages.messages[i]["platform_id"]["fb_id"] = fb_id;
+            break;
+        }
+    }
+
+    // Write to file
+    fs.writeFileSync('db/messages.yaml', yaml.dump(messages), (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+exports.setDiscordId = function(msg_id, dc_id) {
+    // Read messages file into js object
+    let messages = yaml.load(fs.readFileSync('db/messages.yaml', 'utf8'));
+
+    // Reset
+    var i;
+    for (i = messages.numMessages - 1; i >= 0; i--) {
+        if (messages.messages[i]["msg_id"] == msg_id) {
+            messages.messages[i]["platform_id"]["dc_id"] = dc_id;
+            break;
+        }
+    }
+
+    // Write to file
+    fs.writeFileSync('db/messages.yaml', yaml.dump(messages), (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+}
+
+exports.writeMessage = function(body,  senderID, is_fb) {
+
+    let messages = yaml.load(fs.readFileSync('db/messages.yaml', 'utf8'));
+    var msg_id = messages.numMessages;
+
     // Assign message attributes to dictionary
-    var newMsg = {id: messages.numMessages,
+    var newMsg = {msg_id: msg_id,
        body: body,
-       reacts: reacts,
        senderID: senderID,
-       is_fb: is_fb};
+       isFb: is_fb,
+       platform_id: {
+           fb_id: null,
+           dc_id: null
+       },
+       reacts: null,
+       replyTo: null};
 
     // Increment total number of messages stored
     messages.numMessages++;
@@ -23,13 +148,33 @@ exports.writeMessage = function(body, reacts, senderID, is_fb) {
         }
     });
 
-    return;
+    return msg_id;
+}
+
+exports.setReply = function(msg_id, replied_id) {
+    // Read messages file into js object
+    let messages = yaml.load(fs.readFileSync('db/messages.yaml', 'utf8'));
+
+    // Reset
+    var i;
+    for (i = messages.numMessages - 1; i >= 0; i--) {
+        if (messages.messages[i]["msg_id"] == msg_id) {
+            messages.messages[i]["replyTo"] = replied_id;
+        }
+    }
+
+    // Write to file
+    fs.writeFileSync('db/messages.yaml', yaml.dump(messages), (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
 }
 
 exports.clearMessages = function() {
     // Read messages file into js object
     let messages = yaml.load(fs.readFileSync('db/messages.yaml', 'utf8'));
-    console.log(messages);
+
     // Reset message db to empty
     messages.messages = [];
     messages.numMessages = 0;
@@ -40,5 +185,27 @@ exports.clearMessages = function() {
             console.log(err);
         }
     });
-    console.log(messages);
+}
+
+exports.MsgToSender = function (msg_id) {
+    // Read messages file into js object
+    let messages = yaml.load(fs.readFileSync('db/messages.yaml', 'utf8'));
+
+    var i;
+    var ret = null;
+    for (i = messages.numMessages - 1; i >= 0; i--) {
+        if (messages.messages[i]["msg_id"] == msg_id) {
+            ret = messages.messages[i]["senderID"];
+            break;
+        }
+    }
+
+    // Write to file
+    fs.writeFileSync('db/messages.yaml', yaml.dump(messages), (err) => {
+        if (err) {
+            console.log(err);
+        }
+    });
+
+    return ret;
 }
--- index.js
+++ index.js
@@ -14,32 +14,98 @@ const fbThreadID = '100005639376179'
 
 login({email: process.env.FB_EMAIL, password: process.env.FB_PASS}, (err, api) => {
     if(err) return console.error(err);
-    console.log(reacts.reacts[0].unicode)
+
+    // Save facebook id of messenger bot ...
+    const bot_id = api.getCurrentUserID();
+
+    // Discord channel object
+    const dc_channel = discord.channels.get(discordChannelID);
+
+
+    console.log(bot_id)
 
     discord.once('ready', () => {
     	console.log('Discord bot ready!');
     });
 
     helpers.clearMessages();
+    // Set messenger bot to listen to itself ...
+    api.setOptions({selfListen: true});
 
 
     // Monitor events in facebook chat ...
     api.listenMqtt((err, event) => {
         console.log(event.type);
         switch(event.type){
+
             // If message was sent
             case "message":
                 // Check to see if facebook message body is not empty ...
                 if (event.body != undefined) {
-                    helpers.writeMessage(event.body, null, event.senderID, true);
-                    discord.channels.get(discordChannelID).send(event.body);
+                    // Check who message author is
+                    console.log(event.senderID);
+                    console.log(bot_id);
+                    if (event.senderID != bot_id) {
+                        // Write message to database
+                        var msg_id = helpers.writeMessage(event.body, event.senderID, true);
+                        // Log messenger id of message received
+                        helpers.setFacebookId(msg_id, event.messageID);
+                        // Send message in discord and log id of message sent
+                        dc_channel.send(event.body)
+                        .then(
+                            sent => {
+                                helpers.setDiscordId(msg_id, sent.id);
+                            }
+                        );
+                    } else {
+                        var msg_id = helpers.contentToMsgId(event.body);
+                        helpers.setFacebookId(msg_id, event.messageID);
+                    }
+
                 }
                 break;
 
             case "message_reply":
                 // Check to see if facebook message body is not empty ...
                 if (event.body != undefined) {
-                    discord.channels.get(discordChannelID).send(event.body)
+                    if (event.senderID != bot_id) {
+                        // Log message received in database
+                        var msg_id = helpers.writeMessage(event.body, event.senderID, true);
+                        // Id of message being replied to
+                        var replied_id = helpers.IdToMsgId(event.messageReply.messageID);
+
+                        helpers.setReply(msg_id, replied_id);
+                        // Log messenger id of message received
+                        helpers.setFacebookId(msg_id, event.messageID);
+
+
+
+                        dc_channel.fetchMessage(helpers.MsgIdToId(replied_id)["dc_id"])
+                        .then(
+                            replied => {
+                                const replyEmbed = new Discord.RichEmbed()
+                                    .setDescription("**[```"+event.messageReply.body+"```]("+replied.url+")**")
+                                    .addField("\u200B", event.body);
+                                replyEmbed.setURL(replied.url);
+                                dc_channel.send(replyEmbed)
+                                .then(
+                                    sent => {
+                                        helpers.setDiscordId(msg_id, sent.id);
+                                    }
+                                );
+                            }
+                        );
+                        // Send message in discord and log id of message sent
+
+
+                    } else {
+                        console.log(event.body);
+                        var msg_id = helpers.contentToMsgId(event.body);
+                        console.log(msg_id);
+                        console.log(event.messageID);
+                        helpers.setFacebookId(msg_id, event.messageID);
+                    }
+
                 }
                 break;
             // If a message reaction occurred
@@ -52,7 +118,10 @@ login({email: process.env.FB_EMAIL, password: process.env.FB_PASS}, (err, api) =
     discord.on('message', message => {
         // Ignore messages from bot ...
         if (message.author.bot) return;
-        helpers.writeMessage(message.content, null, message.author.id, false);
+
+        // Write message to database
+        var msg_id = helpers.writeMessage(message.content, message.author.id, false);
+        helpers.setDiscordId(msg_id, message.id);
         api.sendMessage(message.content, fbThreadID);
     });
 });
